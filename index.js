49d616a15d86b7562e648bef26c6b79e03ea426a
jousethen <jthen93@gmail.com>
2021-08-11 00:25:50 -0400

    Completed lab. Had to use innerHTML instead of innerText for testing

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+  const data = {
+    name: name,
+    email: email
+  };
+
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  return fetch("http://localhost:3000/users", configObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      let title = document.createElement("p")
+      title.innerHTML = json.id;
+      document.body.appendChild(title);
+
+    })
+    .catch(function (error) {
+      console.log(error);
+      //error.message = "Unauthorized Access";
+      let errorMessage = document.createElement("p")
+      errorMessage.innerHTML = error.message;
+      document.body.appendChild(errorMessage);
+    });
+}
